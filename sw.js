b48c740cd9064e3cb29e5eea3eaef62f2515d900
craig robson <craig.robson@onecalldirect.co.uk>
2019-07-17 09:43:00 +0100

    Some SW code.

--- sw.js
+++ sw.js
@@ -5,3 +5,29 @@ if (workbox) {
 } else {
     console.log(`Boo! Workbox didn't load 😬`);
 }
+
+workbox.setConfig({ debug: true })
+workbox.skipWaiting()
+workbox.clientsClaim()
+
+// google stuff
+// Cache the Google Fonts stylesheets with a stale-while-revalidate strategy.
+workbox.routing.registerRoute(
+    /^https:\/\/fonts\.googleapis\.com/,
+    workbox.strategies.staleWhileRevalidate({ cacheName: 'google-fonts-stylesheets' })
+)
+
+// Cache the underlying font files with a cache-first strategy for 1 year.
+workbox.routing.registerRoute(
+    /^https:\/\/fonts\.gstatic\.com/,
+    workbox.strategies.cacheFirst({
+        cacheName: 'google-fonts-webfonts',
+        plugins  : [
+            new workbox.cacheableResponse.Plugin({ statuses: [0, 200] }),
+            new workbox.expiration.Plugin({
+                maxAgeSeconds: 60 * 60 * 24 * 365,
+                maxEntries   : 30,
+            }),
+        ],
+    })
+)
